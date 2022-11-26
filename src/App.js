35779add03e4e6aa6b5c029d1e141c30112bf55e
Chidimma <chidimmaofodum@gmail.com>
2022-11-26 02:02:44 +0000

    sort toggle button created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import StudentHighScores from './StudentHighScores';
+import StudentHighScores from "./StudentHighScores";
+import { useState } from "react";
 import allCountryScores from "./scores.js";
-import './App.css';
-import sortData from "./sortData"
+import "./App.css";
+import sortData from "./sortData";
 
-sortData(allCountryScores)
+// sortData(allCountryScores)
 
 function App() {
+  const [order, setOrder] = useState(allCountryScores);
+
+  function handleOrder() {
+   if(order[0].name === "Ethiopia") {
+    let copy = [...order].sort((a,b) => a.name < b.name? -1: 1)
+    for(let obj of copy) {
+      let { scores } = obj;
+      scores.sort((a, b) => (a.s > b.s ? -1 : 1));
+      setOrder(copy)
+    }}
+   else setOrder(allCountryScores)
+  }
   return (
     <div className="App">
-     <StudentHighScores allScores = {allCountryScores}  />
+      <button onClick={handleOrder}>Toggle</button>
+      <StudentHighScores allScores={order} />
     </div>
   );
 }
--- src/sortData.js
+++ src/sortData.js
@@ -1,6 +1,6 @@
 function sortData(arr) {
-  arr.sort((a,b) => a.name < b.name? -1: 1);
-  for(let obj of arr) {
+  let copy = [...arr].sort((a,b) => a.name < b.name? -1: 1);
+  for(let obj of copy) {
     let {scores} = obj
     scores.sort((a, b) => (a.s > b.s ? -1 : 1));
   }
